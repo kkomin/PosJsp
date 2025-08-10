83cd681005eb1311a15ebdf8eac54b9708d037ec
kkomin <min_0625@kakao.com>
2025-08-11 01:31:21 +0900

    [⚙️ FEATURE] : DTO에 emp_id 추가

--- PosJspProject/src/main/java/dto/UserDTO.java
+++ PosJspProject/src/main/java/dto/UserDTO.java
@@ -4,14 +4,20 @@ public class UserDTO {
     private String userId;
     private String pw;
     private String userName;
+    private int empId;
 
     public UserDTO() {
     }
 
-    public UserDTO(String userId, String pw, String userName) {
+    public UserDTO(String userId, String pw, String userName, int empId) {
         this.userId = userId;
         this.pw = pw;
         this.userName = userName;
+        this.empId = empId;
+    }
+    
+    public int getEmpId() {
+        return empId;
     }
 
     public String getUserId() {
