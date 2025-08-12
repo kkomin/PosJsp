de641c41e10cdb32e06d90e1ba06fd4dee22ad6c
이다민 <min_0625@kakao.com>
2025-08-12 14:32:57 +0900

    [⚙️ FEATURE] : 제품 등록 DAO 로직 구현

--- PosJspProject/src/main/java/dao/ProductDAO.java
+++ PosJspProject/src/main/java/dao/ProductDAO.java
@@ -1,5 +1,29 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+
+import DB.ConnectDB;
+
 public class ProductDAO {
-	
+	public void insertProduct(String prodName, String company, Date expiration, char isAdult, int price, int stock) {
+		String insertSql = "insert into products (prod_id, prod_name, company, expiration, is_adult, price, stock) values (prod_id_seq.nextval, ?, ?, ?, ?, ?, ?)";
+		
+		try(Connection conn = ConnectDB.getConnectionDB();
+				PreparedStatement pre = conn.prepareStatement(insertSql)) {
+				
+			pre.setString(1, prodName);
+			pre.setString(2, company);
+			pre.setDate(3, expiration);
+			pre.setString(4, String.valueOf(isAdult));
+			pre.setInt(5, price);
+			pre.setInt(6, stock);
+			
+			pre.executeUpdate();
+			
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
 }
