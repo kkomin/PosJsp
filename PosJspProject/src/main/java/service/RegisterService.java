7e4122a24cec878d2f0662e450ed32675a3b668f
이다민 <min_0625@kakao.com>
2025-08-12 15:53:22 +0900

    [⚙️ FEATURE] : 등록 서비스 파일 로직 구현

---
+++ PosJspProject/src/main/java/service/RegisterService.java
@@ -0,0 +1,28 @@
+package service;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import dao.ProductDAO;
+
+public class RegisterService {
+	private ProductDAO productDAO = new ProductDAO(); 
+		public boolean registerProduct(String prodName, String company, String expirationStr, char isAdult, int price, int stock) {
+			try {
+				// 날짜 변환
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyMMdd");
+				LocalDate localDate = LocalDate.parse(expirationStr, formatter);
+				Date expiration = Date.valueOf(localDate);
+				
+				// 유효성 검사 추가 예정
+				
+				productDAO.insertProduct(prodName, company, expiration, isAdult, price, stock);
+				return true;
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+}
