b3f7bb62d67c295f333f94425df0c9349fb745b1
kkomin <min_0625@kakao.com>
2025-08-17 20:33:18 +0900

    [⚙️ FEATURE] : 제품 조회시 회사명으로 검색 DAO 로직 구현

--- PosJspProject/src/main/java/dao/SearchDAO.java
+++ PosJspProject/src/main/java/dao/SearchDAO.java
@@ -14,15 +14,44 @@ public class SearchDAO {
         String sql = "select * from products where upper(prod_name) like ?";
         List<ProductDTO> list = new ArrayList<ProductDTO>();
         
+        if(name == null) name ="";
+        
         try(Connection conn = ConnectDB.getConnectionDB();
                 PreparedStatement pre = conn.prepareStatement(sql)) {
+            pre.setString(1, "%" + name.toUpperCase() + "%");
             ResultSet rs = pre.executeQuery();
             
             while(rs.next()) {
                 ProductDTO product = new ProductDTO(
                         rs.getInt("PROD_ID"), 
-                        rs.getString("PROD_NAME"),
+                        rs.getString("PROD_NAME").trim(),
                         rs.getString("COMPANY"),
+                        rs.getDate("EXPIRATION"),
+                        rs.getString("IS_ADULT").charAt(0),
+                        rs.getInt("PRICE"),
+                        rs.getInt("STOCK"));
+                list.add(product);
+            }
+            
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+    
+    public List<ProductDTO> searchByCompany(String company) {
+        String sql = "select * from products where upper(company) like ?";
+        List<ProductDTO> list = new ArrayList<ProductDTO>();
+        
+        try(Connection conn = ConnectDB.getConnectionDB();
+                PreparedStatement pre = conn.prepareStatement(sql)) {
+            pre.setString(1, "%" + company.toUpperCase() + "%");
+            ResultSet rs = pre.executeQuery();
+            while(rs.next()) {
+                ProductDTO product = new ProductDTO(
+                        rs.getInt("PROD_ID"), 
+                        rs.getString("PROD_NAME"),
+                        rs.getString("COMPANY").trim(),
                         rs.getDate("EXPIRATION"),
                         rs.getString("IS_ADULT").charAt(0),
                         rs.getInt("PRICE"),
